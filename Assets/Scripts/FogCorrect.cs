5f6572103625bb96963d4fd676b088aaf8697841
应彧刚 <yingyugang@yingyugangdeMacBook-Pro.local>
2017-10-30 17:42:23 +0900

    Init fog of war.

---
+++ Assets/Scripts/FogCorrect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogCorrect : MonoBehaviour {
+
+	public Transform fog;
+	public Vector3 offset = new Vector3 (0,3,0);
+	void Start () {
+		
+	}
+	
+	void LateUpdate () {
+		if(fog != null){
+			RaycastHit hit;
+			if (Physics.Raycast (Camera.main.transform.position ,(transform.position + offset -  Camera.main.transform.position).normalized,out hit,Mathf.Infinity)) {
+				fog.transform.position = hit.point;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/MoveAction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAction : MonoBehaviour {
+
+	public Vector3 targetPos;
+	public Vector3 startPos;
+
+	Vector3 mTarget;
+	Vector3 mStart;
+	void Start () {
+		startPos = transform.position;
+		Animation anim = GetComponentInChildren<Animation> ();
+		anim.Play ("Run01");
+		mTarget = targetPos;
+		mStart = startPos;
+	}
+
+	float duration = 3f;
+	float t = 0;
+	void Update () {
+		if(t>=1){
+			Vector3 temp = mTarget;
+			mTarget = mStart;
+			mStart = temp;
+			t = 0;
+		}
+		t += Time.deltaTime / duration;
+		transform.LookAt (mTarget);
+		transform.position = Vector3.Lerp (mStart,mTarget,t);
+	}
+}
